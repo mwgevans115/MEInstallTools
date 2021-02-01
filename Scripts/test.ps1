a7e50ba484be3030cc672eb22f34c855b8a9e902
Mark Evans <mark.evans@mnpthesolution.com>
2021-02-01 08:07:28 +0000

    Remove test scripts

--- Scripts/test.ps1
+++ Scripts/test.ps1
@@ -1,68 +1,80 @@
-#1. Add site to trusted sites
 
-#Setting IExplorer settings
-Write-Verbose "Now configuring IE"
-
-#Navigate to the domains folder in the registry
-set-location "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
-set-location ZoneMap\Domains
-
-#Create a new folder with the website name
-new-item testsite.site.com/ -Force #website part without https
-set-location testsite.site.com/
-new-itemproperty . -Name https -Value 2 -Type DWORD -Force
-
-Write-Host "Site added Successfully"
-Start-Sleep -s 2
-
-# 2. Disable IE protected mode
-
-# Disabling protected mode and making level 0
-
-#Zone 0 – My Computer
-#Zone 1 – Local Intranet Zone
-#Zone 2 – Trusted sites Zone
-#Zone 3 – Internet Zone
-#Zone 4 – Restricted Sites Zone
-
-#“2500” is the value name representing “Protected Mode” tick. 3 means Disabled, 0 – Enabled
-
-#Disable protected mode for all zones
-Set-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1" -Name 2500 -Value "3"
-Set-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2" -Name 2500 -Value "3"
-Set-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" -Name 2500 -Value "3"
-Set-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4" -Name 2500 -Value "3"
-
-Write-Host "IE protection mode turned Off successfully"
-Start-Sleep -s 2
-
-# 3. Bring down security level for all zones
-
-#Set Level 0 for low
-Set-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\1" -Name 1A10 -Value "0"
-Set-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\2" -Name 1A10 -Value "0"
-Set-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" -Name 1A10 -Value "0"
-Set-ItemProperty -path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\4" -Name 1A10 -Value "0"
-
-Stop-Process -name explorer
-
-function Disable-InternetExplorerESC {
-    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
-    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-    Stop-Process -Name Explorer
-    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+Add-LoggingTarget -Name Console -Configuration @{Level = 'DEBUG' }
+$webConfig = (get-content C:\wwwroot\oms-admin\Web.config) -as [XML]
+Write-Log -Level INFO -Message "Documenting web.config settings"
+@('AuthMode', 'Environment.AllowDebugProfiler', 'Environment.IsTest',
+    'ValidationExpression_Code', 'ValidationExpression_Sku',
+    'ValidationExpression_SourceCode', 'Validation_ForceNewCodesUppercase',
+    'AppSecrets.UseMachineEncryptionVector', 'AppSecrets.AllowPasswordsInMemory',
+    'UI.Active', 'Pages.Active') | ForEach-Object {
+    $node = $webConfig.SelectSingleNode("//appSettings/add[@key='$_']")
+    Write-Log -Level DEBUG -Message "`t{0} : {1}" -Arguments $node.key, $node.value
 }
-function Enable-InternetExplorerESC {
-    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 1
-    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 1
-    Stop-Process -Name Explorer
-    Write-Host "IE Enhanced Security Configuration (ESC) has been enabled." -ForegroundColor Green
+
+Write-Log "Setting Connection Strings"
+$ConnectionSecrets = (Get-Content 'C:\wwwroot\oms-admin\ConnectionStrings.secret') -as [XML]
+$ConnectionStrings = $ConnectionSecrets.SelectSingleNode("//connectionStrings")
+@(@{Name = 'MNPUserMaster'; Database = 'MNPUserMaster'; ProviderName = "System.Data.SqlClient" },
+    @{Name = 'Admin'; Database = 'OrderActive'; ProviderName = "System.Data.SqlClient" }) | ForEach-Object {
+    $Builder = New-Object -TypeName 'System.Data.SqlClient.SqlConnectionStringBuilder'
+    $builder["Data Source"] = "localhost"
+    $builder["integrated Security"] = $true
+    $builder["Initial Catalog"] = "$($_.Database)"
+    $Builder["Persist Security Info"] = $true
+    $Builder["User ID"] = "OrderActive"
+    $Builder["Application Name"] = "OMSAdmin"
+    $node = $ConnectionStrings.SelectSingleNode("//add[@name='$($_.Name)']")
+    If (!($node)) {
+        $node = $ConnectionStrings.AppendChild($ConnectionSecrets.CreateElement("add"))
+    }
+    $node.SetAttribute("name", "$($_.Name)")
+    $node.SetAttribute("connectionString", $Builder.ConnectionString)
+    $node.SetAttribute("providerName", $_.ProviderName)
 }
-function Disable-UserAccessControl {
-    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 00000000
-    Write-Host "User Access Control (UAC) has been disabled." -ForegroundColor Green
+$ConnectionSecrets.Save('C:\wwwroot\oms-admin\ConnectionStrings.secret')
+
+Write-Log "Setting appSettings Secrets"
+$AppSettingsSecrets = (Get-Content 'C:\wwwroot\oms-admin\AppSettings.secret') -as [XML]
+$AppSettings = $AppSettingsSecrets.SelectSingleNode("//appSettings")
+@(@{key="Admin.Password";value=""},
+@{key="MNPUserMaster.Password";value=""}) | ForEach-Object {
+    $node = $AppSettings.SelectSingleNode("//add[@key='$($_.key)']")
+    If (!($node)) {
+        $node = $AppSettings.AppendChild($AppSettingsSecrets.CreateElement("add"))
+    }
+    $node.SetAttribute("key", $_.key)
+    $node.SetAttribute("value", $_.value)
 }
+$AppSettingsSecrets.Save('C:\wwwroot\oms-admin\AppSettings.secret')
+$R = Invoke-WebRequest 'http://localhost:8080/Home/Encrypt' -SessionVariable Session
+# This command stores the first form in the Forms property of the $R variable in the $Form variable.
+$Form = $R.Forms[0]
+# These commands populate the string to encrypt and the passwordmode of the respective Form fields.
+$Form.Fields["StringToEncrypt"] = "Password"
+$Form.Fields["PasswordMode"] = $true
+# This command creates the Uri that will be used to log in to facebook.
+# The value of the Uri parameter is the value of the Action property of the form.
+#$Uri = "https://www.facebook.com" + $Form.Action
+# Now the Invoke-WebRequest cmdlet is used to sign into the Facebook web service.
+# The WebRequestSession object in the $FB variable is passed as the value of the WebSession parameter.
+# The value of the Body parameter is the hash table in the Fields property of the form.
+# The value of the *Method* parameter is POST. The command saves the output in the $R variable.
+$R = Invoke-WebRequest -Uri 'http://localhost:8080/Home/Encrypt' -WebSession $Session -Method POST -Body $Form.Fields
+if ($R.StatusDescription -eq 'OK') {
+    if ($r.Content -match '(?<=<pre>).*?(?=</pre>)') {
+        $EncryptedPassword = $Matches[0]
+    }
+}
+Write-Log "Updating appSettings Secrets with encrypted password"
+$AppSettingsSecrets = (Get-Content 'C:\wwwroot\oms-admin\AppSettings.secret') -as [XML]
+$AppSettings = $AppSettingsSecrets.SelectSingleNode("//appSettings")
+@(@{key="Admin.Password";value="$EncryptedPassword"},
+@{key="MNPUserMaster.Password";value="$EncryptedPassword"}) | ForEach-Object {
+    $node = $AppSettings.SelectSingleNode("//add[@key='$($_.key)']")
+    If (!($node)) {
+        $node = $AppSettings.AppendChild($AppSettingsSecrets.CreateElement("add"))
+    }
+    $node.SetAttribute("key", $_.key)
+    $node.SetAttribute("value", $_.value)
+}
+$AppSettingsSecrets.Save('C:\wwwroot\oms-admin\AppSettings.secret')
--- Scripts/test1.ps1
+++ Scripts/test1.ps1
@@ -1,100 +1,52 @@
-[CmdletBinding(DefaultParameterSetName = 'USER+PASSWORD')]
-param (
-    [Parameter(ParameterSetName = 'USER+PASSWORD')]
-    [String]
-    $OrderActiveUsername = 'MarkEvans',
-    [Parameter(ParameterSetName = 'USER+PASSWORD')]
-    [SecureString]$OrderActiveSecurePassword
-)
-Add-LoggingTarget -Name Console -Configuration @{Level = 'DEBUG'; Format = '[%{timestamp:+%T} %{level:-7}] %{message}' }
-#region orderactivecredentials
-$SaveCredentials = $true
-If ($PSCmdlet.ParameterSetName -eq 'USER+PASSWORD') {
-    if (!($OrderActiveSecurePassword)) {
-        $CredentialManagerCredential = Get-CredentialManagerCredential -Target 'MNP' -User $OrderActiveUserName
-        if ($CredentialManagerCredential.User) {
-            Write-Log -Level INFO -Message 'Using Stored Credential'
-            Write-Log -Level DEBUG -Message "`tTarget : {0}" -Arguments $CredentialManagerCredential.Target
-            Write-Log -Level DEBUG -Message "`tUser   : {0}" -Arguments $CredentialManagerCredential.User
-            Write-Log -Level DEBUG -Message "`tComment: {0}" -Arguments $CredentialManagerCredential.Comment
-            $OrderActiveSecurePassword = $CredentialManagerCredential.SecurePass
-            $SaveCredentials = $false
-        }
-        else {
-            Write-Output "Enter Password for $OrderActiveUserName (Leave Blank to generate)"
-            $OrderActiveSecurePassword = Read-Host -AsSecureString
-            If ($OrderActiveSecurePassword.Length -eq 0) {
-                Write-Log -Level WARNING -Message 'Generating Password for SQL Login "{0}"' -Arguments $OrderActiveUser
-                $OrderActiveSecurePassword = Get-NewPassword
-            }
-            else {
-                Write-Log -Level INFO -Message 'Using Input Password for SQL Login "{0}"' -Arguments $OrderActiveUser
-            }
-        }
-    }
-    $OrderActiveUserCredential = New-Object System.Management.Automation.PSCredential ($OrderActiveUsername, $OrderActiveSecurePassword)
-}
-if ($SaveCredentials) {
-    Write-Log -Level WARNING -Message "Saving Credentials for {0} to Credential Manager" -Arguments $OrderActiveUserCredential.UserName
-    $CredentialManagerCredential = Set-CredentialManagerCredential -Target 'MNP' -UserCredential $OrderActiveUserCredential -Comment "Set by install script $(Get-Date)"
-    Write-Log -Level DEBUG -Message "`tTarget : {0}" -Arguments $CredentialManagerCredential.Target
-    Write-Log -Level DEBUG -Message "`tUser   : {0}" -Arguments $CredentialManagerCredential.User
-    Write-Log -Level DEBUG -Message "`tComment: {0}" -Arguments $CredentialManagerCredential.Comment
-}
-#endregion orderactivecredentials
+# Author: Shawn Keene, adapted from Rhys Campbell http://www.youdidwhatwithtsql.com/automating-internet-explorer-with-powershell/467/
+# Date: 8/26/2015
+# Powershell script to automate Internet Explorer
 
-$SQLParams = @{
-    ServerInstance = 'localhost'
-}
-#region Checking/Creating OrderActive User on databases
-Write-Log -Level INFO -Message "Checking/Creating Login on {0}" -Arguments $SQLParams.ServerInstance
-$Login = Get-SqlLogin @SQLParams -LoginName $OrderActiveUserCredential.UserName -ErrorAction SilentlyContinue
-If ($Login) {
-    Write-Log -Level INFO -Message "`tLogin {0} Exists" -Arguments $OrderActiveUserCredential.UserName
-    try {
-        Invoke-Sqlcmd @SQLParams -Query 'Select ''String''' -Credential $OrderActiveUserCredential -ErrorAction SilentlyContinue
-    }
-    catch {
-        if (Invoke-Sqlcmd @SQLParams -Query "Exec sp_who" | Where-Object {$_.ItemArray[3] -eq $OrderActiveUserCredential.UserName}){
-            Write-Log -Level ERROR -Message "`tIncorrect Credentials and User {0} Logged In" -Arguments $OrderActiveUserCredential.UserName
-            $PreRequisites = $false
-        } else {
-            Remove-SqlLogin @SQLParams -LoginName $OrderActiveUserCredential.UserName -Force
-            Write-Log -Level WARNING -Message "`tIncorrect Credentials - Removing Login {0}" -Arguments $OrderActiveUserCredential.UserName
-        }
-    }
-}
-$Login = Get-SqlLogin @SQLParams -LoginName $OrderActiveUserCredential.UserName -ErrorAction SilentlyContinue
-If (!($Login)) {
-    Write-Log -Level WARNING -Message "`tCreating New Login {0}" -Arguments $OrderActiveUserCredential.UserName
-    Add-SqlLogin @SQLParams -LoginPSCredential $OrderActiveUserCredential `
-        -Enable -LoginType SqlLogin -EnforcePasswordExpiration:$false `
-        -DefaultDatabase 'OrderActive' -GrantConnectSql -MustChangePasswordAtNextLogin:$false | Out-Null
-}
-$role = 'db_owner'
-@('OrderActive', 'MNPServiceCfg', 'MNPCalendar', 'MNPUserMaster') | ForEach-Object {
-    $userRoles = Get-DBLogin -Login $OrderActiveUserCredential.UserName -Database $_ -OutputAs DataTables
-    $dvRoles = New-Object System.Data.DataView($userRoles)
-    $dvRoles.RowFilter = "Isnull(DBUserName,'') = ''"   #"DBUserName <> ''"
-    if ($dvRoles.Count -gt 0){
-        Write-Log -Level WARNING "`tAdding User {0} for Login {1} on Database {2}" -Arguments $OrderActiveUserCredential.UserName,$OrderActiveUserCredential.UserName,$_
-        $Query = "DROP USER IF EXISTS $($OrderActiveUserCredential.UserName); CREATE USER $($OrderActiveUserCredential.UserName) FOR LOGIN $($OrderActiveUserCredential.UserName);"
-        Invoke-Sqlcmd @SQLParams -Database $_ -Query $Query
-    } else {
-        Write-Log -Level DEBUG -Message "`tUser {0} for Login {1} on Database {2} {3}" -Arguments $OrderActiveUserCredential.UserName,$OrderActiveUserCredential.UserName,$_,'EXISTS'
-    }
-    $dvRoles.RowFilter = "DBUserRole = 'db_owner'"
-    if ($dvRoles.Count -gt 0) {
-        Write-Log -Level DEBUG -Message "`tUser {0} in db_owner role on Database {1}" -Arguments $OrderActiveUserCredential.UserName,$_
-    }
-    else {
-        Write-Log -Level WARNING -Message "`tUser {0} not in db_owner role on Database {1}" -Arguments $OrderActiveUserCredential.UserName,$_
-        $Query = "ALTER ROLE [db_owner] ADD MEMBER [$($OrderActiveUserCredential.UserName)]"
-        Invoke-Sqlcmd @SQLParams -Database $_ -Query $Query
-    }
-}
-#endregion
+cls
+$username = "sarah";
+$password = "monkey";
+$loginUrl = "http://example.com";
+$shopID = "A1C";
+$AddProdURL = "https://example.com/global/ProductAdd";
+$iterator = 1;
 
-Wait-Logging
-Invoke-Sqlcmd @SQLParams -Query 'Select ''String''' -Credential $OrderActiveUserCredential
+#initialize browser
+$ie = New-Object -com internetexplorer.application;
+$ie.visible = $true;
+$ie.navigate($loginUrl);
+while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
 
+#login
+($ie.document.getElementsByName("username") |select -first 1).value = $username;
+($ie.document.getElementsByName("password") |select -first 1).value = $password;
+($ie.document.getElementsByName("login") |select -first 1).click();
+while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
+
+#choose shop
+($ie.document.getElementsByName("shop") |select -first 1).value = $shopID;
+($ie.document.getElementsByName("submit") |select -first 1).click();
+while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
+
+start-sleep -seconds 2
+
+$products = import-csv products.csv | foreach {
+    write-host Product $iterator -  $_.ITEM_NUMBER
+
+    #go to product addition form
+    $ie.navigate($AddProdURL);
+    while ($ie.Busy -eq $true) { Start-Sleep -Seconds 1; }    #wait for browser idle
+
+    #fill out form fields
+    ($ie.document.getElementsByName("item_number") |select -first 1).value = $_.ITEM_NUMBER;
+    ($ie.document.getElementsByName("item_name") |select -first 1).value = $_.ITEM_NAME;
+    ($ie.document.getElementsByName("item_type") |select -first 1).value = "C";
+    ($ie.document.getElementsByName("short_description") |select -first 1).value = $_.ShortProductDescription;
+    ($ie.document.getElementsByName("long_description") |select -first 1).value = $_.LongDescription;
+    ($ie.document.getElementsByName("legal") |select -first 1).click();
+    ($ie.document.getElementsByName("legal_text") |select -first 1).value = $_.LegalText;
+    ($ie.document.getElementsByName("submit") |select -first 1).click();
+
+    $iterator = $iterator+1;
+}
+
+Write-Host -ForegroundColor Green "All Done!";
