821593828ca95124be1d460953cb3de45fdf1f18
Mark Evans <mark.evans@mnpthesolution.com>
2021-02-02 15:47:28 +0000

    Streamlined Install Modules Function

--- src/Public/Install-Modules.ps1
+++ src/Public/Install-Modules.ps1
@@ -1,7 +1,7 @@
 function Install-Modules {
     param (
         [String[]]
-        $Modules=@('PackageManagement','Logging','SqlServer','SharePointPnPPowerShellOnline'),
+        $Modules=@('Logging','SqlServer','SharePointPnPPowerShellOnline'),
         [ValidateSet("CurrentUser", "AllUsers")]
         $Scope = "CurrentUser"
     )
@@ -10,15 +10,18 @@ function Install-Modules {
         Scope = $Scope
         AllowClobber = $true
     }
-    Install-PackageProvider NuGet -Force -Scope $Scope
-    $Modules  | ForEach-Object {
+    @('PackageManagement') + $Modules  | ForEach-Object {
         $LatestVersion = [Version](Find-Module -Name $_).Version
         $CurrentVersion = Get-Module -ListAvailable -Name $_ | ForEach-Object { [Version]$_.Version } | Sort-Object $_ | Select-Object -Last 1
         IF ($LatestVersion -gt $CurrentVersion) {
             Write-Warning "Installing Module $_ Version $($LatestVersion.ToString())"
+            if ($_ -eq 'PackageManagement'){Install-PackageProvider NuGet -Force -Scope $Scope}
             Install-Module @InstallModuleParams -Name $_
         } else {
             Write-Debug "Modile $_ Version $($CurrentVersion.ToString()) already installed"
         }
+        Remove-Module -Name $_ -Force -ErrorAction SilentlyContinue
+        Import-Module -Name $_ -Force
     }
 }
+#Install-Modules -Modules @('Logging')
